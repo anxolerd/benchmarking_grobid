e8276ed6833e2fa47fb1ed2fd04563f3bddeb83a
Oleksandr Kovalchuk <anxolerd@outlook.com>
2022-09-15 22:58:27 +0300

    WIP biorxiv dataset

--- retrieve_datasets.sh
+++ retrieve_datasets.sh
@@ -17,14 +17,28 @@ git clone git@github.com:arefinnomi/curriculum_vitae_data
 # Retrieve technical articles, manuals and slides data
 git clone git@github.com:tpn/pdfs
 
+# Retrieve biorxiv data
+mkdir -p biorxiv
+cd biorxiv
+for purpose in "test-2000" "train-6000" "validation-2000"; do
+  wget -c https://zenodo.org/record/3873702/files/biorxiv-10k-${purpose}.zip?download=1 -O ${purpose}.zip
+  mkdir -p ${purpose}
+  unzip ${purpose}.zip -d ${purpose}
+done
+cd ../
+
 
 # Create datasets direcories
-mkdir -p datasets/{arxiv,cv,tech}
+mkdir -p datasets/{arxiv,cv,tech,biorxiv,biorxiv-test,biorxiv-train,biorxiv-validation}
 
 # Hardlink pdfs from datasets to corresponding directories
 find curriculum_vitae_data/ -name '*.pdf' -print -exec ln {} datasets/cv/ \;
 find arxiv-all/ -name '*.pdf' -print -exec ln {} datasets/arxiv/ \;
 find pdfs/ -name '*.pdf' -print -exec ln {} datasets/tech/ \;
+find biorxiv/ -name '*.pdf' -print -exec ln {} datasets/biorxiv/ \;
+find biorxiv/test-2000/ -name '*.pdf' -print -exec ln {} datasets/biorxiv-test/ \;
+find biorxiv/train-6000/ -name '*.pdf' -print -exec ln {} datasets/biorxiv-train/ \;
+find biorxiv/validation-2000/ -name '*.pdf' -print -exec ln {} datasets/biorxiv-validation/ \;
 
 # Done, print datasets sizes
 echo -e 'Dataset\tSamples'
